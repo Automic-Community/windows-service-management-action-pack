c18d725ec60319e1779d99221b7848972c2a404a
Huy.Tran-Quang <tqh@automic.com>
2015-11-10 11:06:18 +0700

    fix test case with iniate data

--- test/ride.bat
+++ test/ride.bat
@@ -2,9 +2,4 @@ del .\output\*.xml /Q
 del .\output\*.html /Q
 del .\output\*.png /Q
 
-set CLASSPATH="%~dp0\..\..\lib\itpa-keywords-0.0.1-SNAPSHOT.jar;%CLASSPATH%"
-REM set CLASSPATH="C:\Workspace\lqt_sara_main\Projects\itpa\keywords\target\itpa-keywords-0.0.1-SNAPSHOT.jar;%CLASSPATH%"
-
-echo %CLASSPATH%
-
-ride.py ./
+open_ride.cmd %cd%
